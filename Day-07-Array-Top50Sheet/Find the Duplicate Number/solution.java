827322a0fc45b3f56132c7c9d70cafb94aafca76
Shubham Kumar <145495342+Shubhthakur42@users.noreply.github.com>
2026-04-08 23:18:12 +0530

    Refactor findDuplicate method for readability
    Refactor the findDuplicate method to improve readability by removing unnecessary empty lines.

--- Day-07-Array-Top50Sheet/Find the Duplicate Number/solution.java
+++ Day-07-Array-Top50Sheet/Find the Duplicate Number/solution.java
@@ -2,20 +2,17 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
-
         // Step 1: Detect cycle
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
-
         // Step 2: Find duplicate (cycle start)
         slow = nums[0];
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
-
         return slow;
     }
 }
